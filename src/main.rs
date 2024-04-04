4f01376d0367ed1c75cd8962435abbdb05de7d5b
monkeysprocket <mjquinn88@outlook.com>
2024-04-04 21:54:30 +0100

    refactored argument parsing into a function and stored parsed args in a new struct.

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,27 @@ use std::{env, fs};
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = Config::new(&args);
 
-    println!("Searaching for {}", query);
-    println!("In file {}", file_path);
+    println!("Searaching for {}", config.query);
+    println!("In file {}", config.file_path);
 
-    let contents = fs::read_to_string(file_path)
+    let contents = fs::read_to_string(config.file_path)
         .expect("Should have been able to read the file");
 
     println!("With text:\n{contents}");
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+    
+        Config { query, file_path }
+    }
 }
