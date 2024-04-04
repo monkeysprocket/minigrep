b76dca2425007541f0e4974c7525fdc1085756f8
monkeysprocket <mjquinn88@outlook.com>
2024-04-04 22:20:27 +0100

    improved error handling in run

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::{env, fs};
 use std::process;
+use std::error::Error;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -12,7 +13,10 @@ fn main() {
     println!("Searaching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    run(config);
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
 }
 
 struct Config {
@@ -33,9 +37,10 @@ impl Config {
     }
 }
 
-fn run(config: Config) {
-    let contents = fs::read_to_string(config.file_path)
-    .expect("Should have been able to read the file");
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
 
     println!("With text:\n{contents}");
+
+    Ok(())
 }
